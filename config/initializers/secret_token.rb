6ede7f101c5e08b5fd46e467a313864f7f9a1d6b
jeesong <jeewsong@gmail.com>
2013-11-25 15:43:28 -0800

    Starting the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrafficEstimatorApp::Application.config.secret_token = 'be40b1fee9f3ddcaaada44a47da6f0a0e22af7b3ae879f5290f7d6d098217fab2d03e613dfcbf728e712de20c5087dc93a85434a70e33fae2c646a9752df7317'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+TrafficEstimatorApp::Application.routes.draw do
+  root to: "home#index"
+end
